e2d3992626f6443c0187af83c898c3d2a3f5c00a
manas-sahoo-au6 <rmanas000@gmail.com>
2020-05-19 23:51:05 +0530

    fb

--- src/App.js
+++ src/App.js
@@ -31,30 +31,7 @@ class App extends Component {
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
 
-        <div
-          class="fb-post"
-          data-href="https://www.facebook.com/ranjan.manas.7/posts/2981925478583655"
-          data-show-text="true"
-          data-width=""
-        >
-          <blockquote
-            cite="https://developers.facebook.com/ranjan.manas.7/posts/2981925478583655"
-            class="fb-xfbml-parse-ignore"
-          >
-            <p>
-              https://covid19-tracker-reactapp-by-manas.netlify.app Here is my
-              coontribution by building an web application using...
-            </p>
-            Posted by{" "}
-            <a href="https://www.facebook.com/ranjan.manas.7">
-              Ranjan Manas Er
-            </a>{" "}
-            on&nbsp;
-            <a href="https://developers.facebook.com/ranjan.manas.7/posts/2981925478583655">
-              Tuesday, 19 May 2020
-            </a>
-          </blockquote>
-        </div>
+     
       </div>
     );
   }
