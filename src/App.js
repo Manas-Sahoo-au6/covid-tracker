16493f9e3d1853a908f4f87f76f184a715c31ea4
manas-sahoo-au6 <rmanas000@gmail.com>
2020-05-19 23:46:07 +0530

    fb

--- src/App.js
+++ src/App.js
@@ -1,46 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {Cards, Chart,CountryPicker} from './components';
-import styles from './App.module.css';
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 
-import {fetchData}  from './api'
+import { fetchData } from "./api";
 
 class App extends Component {
-
-  state={
+  state = {
     data: {},
-    country: '',
-
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchdata = await fetchData();
+    this.setState({ data: fetchdata });
   }
- async componentDidMount() {
-   const fetchdata = await fetchData();
-   this.setState({data: fetchdata});
- }
 
   handleCountryChange = async (country) => {
-   console.log(country)
-   const fetchdata = await fetchData(country);
-   this.setState({data:fetchdata,country:country});
+    console.log(country);
+    const fetchdata = await fetchData(country);
+    this.setState({ data: fetchdata, country: country });
+  };
 
- }
-
-
-
-  render() { 
-   
-     let {data,country} = this.state;
-    return ( 
+  render() {
+    let { data, country } = this.state;
+    return (
       <div className={styles.container}>
-       <img src="https://i.ibb.co/7QpKsCX/image.png" alt=""/>
-       <h2> Covid-19 Tracker By Manas</h2>
-        <Cards data  = {data} />
-        <CountryPicker handleCountryChange = {this.handleCountryChange}/>
-        <Chart  data  = {data} country={country}/>
-        
-        
+        <img src="https://i.ibb.co/7QpKsCX/image.png" alt="" />
+        <h2> Covid-19 Tracker By Manas</h2>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+
+        <div
+          class="fb-post"
+          data-href="https://www.facebook.com/ranjan.manas.7/posts/2981925478583655"
+          data-show-text="true"
+          data-width=""
+        >
+          <blockquote
+            cite="https://developers.facebook.com/ranjan.manas.7/posts/2981925478583655"
+            class="fb-xfbml-parse-ignore"
+          >
+            <p>
+              https://covid19-tracker-reactapp-by-manas.netlify.app Here is my
+              coontribution by building an web application using...
+            </p>
+            Posted by{" "}
+            <a href="https://www.facebook.com/ranjan.manas.7">
+              Ranjan Manas Er
+            </a>{" "}
+            on&nbsp;
+            <a href="https://developers.facebook.com/ranjan.manas.7/posts/2981925478583655">
+              Tuesday, 19 May 2020
+            </a>
+          </blockquote>
+        </div>
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
